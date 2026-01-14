84afc302c0c1d0f9ef3729396021cac4145e14e2
Logan <logankeenan55@gmail.com>
2026-01-14 13:29:47 -0500

    Started making terrain mesh
    Really broken but looks cool

--- Terrain.cs
+++ Terrain.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 public partial class Terrain : Node3D {
@@ -11,6 +12,8 @@ public partial class Terrain : Node3D {
     [Export] bool randomizeSeed = false;
     private double time = 1;
     private double[,] pointArr;
+    private Vector3[] vertArr;
+    private List<int> indicieList;
     public override void _Ready()
 	{
 
@@ -29,25 +32,68 @@ public partial class Terrain : Node3D {
 
     public void buildTerrain(bool randomizeSeed) {
         pointArr = new double[size,size];
+        vertArr = new Vector3[size*size];
+        indicieList = new();
 
         if(randomizeSeed) {noise.Seed = (int)GD.Randi();}
 
-        for(int x = 0; x < size; x++) {
-            for(int z = 0; z < size; z++) {
-
+        int totalIndex = 0;
+        for(int x = 0; x < size-1; x++) {
+            for(int z = 0; z < size-1; z++) {
+                
+                //2D double array of all points, value is height
                 float noisePos = noise.GetNoise2D(x,z);
                 pointArr[x,z] = (double)(noisePos*amplitude);
 
+                //1D Vector3 array of verticies
+                vertArr[totalIndex] = new Vector3(x,(float)(noisePos*amplitude),z);
+                totalIndex++;
+
+
                 //creat balls for debugging
+                /*
                 MeshInstance3D ball = new MeshInstance3D();
                 ball.Mesh = new SphereMesh() {Radius = 0.3f};
                 AddChild(ball);
                 ball.Position = new Vector3(x,(float)(noisePos*amplitude),z);
+                */
+            }
+        }
+        //list of all indicies
+        for(int x = 0; x < size; x++) {
+            for(int z = 0; z < size; z++) {
+                int a = x*size+z;
+                int b = (x+1)*size+z;
+                int c = x*size+(z+1);
+                int d = (x+1)*size+(z+1);
 
-                
+                indicieList.Add(b); indicieList.Add(a); indicieList.Add(d);
+                indicieList.Add(a); indicieList.Add(d); indicieList.Add(c);
 
             }
         }
+        buildMesh(vertArr,indicieList);
+       
+    }
+    public void buildMesh(Vector3[] vertices, List<int> indicies) {
+        int[] indexArray = indicies.ToArray();
+        var arrays = new Godot.Collections.Array();
+
+        arrays.Resize((int)Mesh.ArrayType.Max);
+
+        arrays[(int)Mesh.ArrayType.Vertex] = vertices;
+        arrays[(int)Mesh.ArrayType.Index] = indexArray;
+
+        var mesh = new ArrayMesh();
+
+        mesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles,arrays);
+
+        var meshInstance = new MeshInstance3D();
+        meshInstance.Mesh = mesh;
+        AddChild(meshInstance);
+
+
+
     }
 
 }
